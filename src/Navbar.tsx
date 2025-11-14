446aa91d52ed78a00d09841c14c2580462468a11
Eric <hayter.eric@gmail.com>
2025-11-13 22:23:04 -0500

    Remove borders on navbar buttons

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="join join-horizontal absolute top-4 left-1/2 -translate-x-1/2 z-10 w-96">
       <details className="dropdown">
-        <summary className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200">
+        <summary className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
           <FilterIcon />
         </summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
@@ -13,11 +13,11 @@ function Navbar() {
           <li><a>Item 2</a></li>
         </ul>
       </details>
-      <label className="input join-item input-lg flex items-center gap-2 shadow-lg text-sm focus:outline-none focus:bg-base-200">
-        <input type="search" required placeholder="Search" className="grow focus:outline-none focus:bg-base-200" />
+      <label className="input join-item input-lg flex items-center gap-2 shadow-lg text-sm focus-within:outline-none focus-within:ring-0 border-none">
+        <input type="search" required placeholder="Search" className="grow focus:outline-none focus:ring-0 outline-none" />
       </label>
       {/* Search button */}
-      <button className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200">
+      <button className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
         <SearchIcon />
       </button>
     </div>
