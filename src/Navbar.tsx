ff595d625f1234b3ac7c7e68386b2a7bfedb35e1
Eric <hayter.eric@gmail.com>
2025-11-13 22:36:48 -0500

    Fix shadow consistency issue in navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="join join-horizontal absolute top-4 left-1/2 -translate-x-1/2 z-10 w-96">
       <details className="dropdown">
-        <summary className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
+        <summary className="btn join-item btn-lg shadow-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
           <FilterIcon />
         </summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
@@ -14,10 +14,10 @@ function Navbar() {
         </ul>
       </details>
       <label className="input join-item input-lg flex items-center gap-2 shadow-lg text-sm focus-within:outline-none focus-within:ring-0 border-none">
-        <input type="search" required placeholder="Search" className="grow focus:outline-none focus:ring-0 outline-none" />
+        <input type="search" required placeholder="Search" className="grow focus:outline-none focus:ring-0 outline-none shadow-lg" />
       </label>
       {/* Search button */}
-      <button className="btn join-item btn-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
+      <button className="btn join-item btn-lg shadow-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
         <SearchIcon />
       </button>
     </div>
