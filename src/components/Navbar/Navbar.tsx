377c29d93691317673d74411eaf3e2b8a0616ee4
Eric <hayter.eric@gmail.com>
2025-12-07 10:04:30 -0500

    Update navbar to use styling similar to rest of the app

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,21 +3,21 @@ import SearchIcon from '../icons/SearchIcon';
 
 function Navbar() {
   return (
-    <div className="join join-horizontal absolute top-4 left-1/2 -translate-x-1/2 z-10 w-96">
+    <div className="join join-horizontal absolute top-4 left-1/2 -translate-x-1/2 z-10 w-96 shadow-lg">
       <details className="dropdown">
-        <summary className="btn join-item btn-lg shadow-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
+        <summary className="btn join-item btn-sm btn-soft">
           <FilterIcon />
         </summary>
-        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-lg">
           <li><a>Item 1</a></li>
           <li><a>Item 2</a></li>
         </ul>
       </details>
-      <label className="input join-item input-lg flex items-center gap-2 shadow-lg text-sm focus-within:outline-none focus-within:ring-0 border-none">
-        <input type="search" required placeholder="Search" className="grow focus:outline-none focus:ring-0 outline-none shadow-lg" />
+      <label className="input join-item input-sm flex items-center gap-2 text-sm">
+        <input type="search" required placeholder="Search" className="grow" />
       </label>
       {/* Search button */}
-      <button className="btn join-item btn-lg shadow-lg hover:bg-primary hover:shadow-md transition-all duration-200 border-none">
+      <button className="btn join-item btn-sm btn-soft btn-primary">
         <SearchIcon />
       </button>
     </div>
